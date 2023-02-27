ed148b63e1dce8b5f88bd079534f488e0467c6d8
Grenish rai <ariestheracer@gmail.com>
2023-02-27 23:15:13 +0530

    script for popup boxes

---
+++ script.js
@@ -0,0 +1,23 @@
+const searchbtn = document.getElementById('searchBtn');
+const closebtn = document.getElementById('closeBtn');
+const searchbox = document.getElementById('hiddenBox');
+
+const subbtn = document.getElementById('subBtn');
+const letterBox = document.getElementById('letterBox');
+const clsLetter = document.getElementById('clsLetter');
+
+searchbtn.addEventListener('click', () => {
+    searchbox.style.display = 'block';
+});
+
+closebtn.addEventListener('click', () => {
+    searchbox.style.display = 'none';
+});
+
+subbtn.addEventListener('click', () => {
+    letterBox.style.display = 'block';
+});
+
+clsLetter.addEventListener('click', () => {
+    letterBox.style.display = 'none';
+});
