db954e16f24766d8c05f724c7762f5f5c2240d1c
Grenish rai <107925840+Grenish@users.noreply.github.com>
2023-03-01 22:41:13 +0530

    Blog pages (#4)
    * Fixes some bugs
    
    * added some images for use
    
    * New page for 404 error, Coming soon and dummy blogs
    
    * Carousel is added
    
    * Photos for carousel
    
    * links used for the material/assets
    
    * carousel css
    
    * script for carousel
    
    * update
    
    * updates
    
    * update
    
    * name change
    
    * error fixed
    
    * update

--- script.js
+++ script.js
@@ -23,24 +23,6 @@ clsLetter.addEventListener("click", () => {
   letterBox.style.display = "none";
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  new Splide("#image-carousel", {
-    width: "100vw",
-    height: "60vh",
-    autoplay: true,
-    loop: true,
-    rewind: true,
-    cover: true,
-    heightRatio: 0.5,
-    heightRatio: 0.5,
-  }).mount();
-});
-
-});
-
-clsLetter.addEventListener("click", () => {
-  letterBox.style.display = "none";
-});
 
 document.addEventListener("DOMContentLoaded", function () {
   new Splide("#image-carousel", {
@@ -55,4 +37,16 @@ document.addEventListener("DOMContentLoaded", function () {
   }).mount();
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide("#image-carousel", {
+    width: "100vw",
+    height: "60vh",
+    autoplay: true,
+    loop: true,
+    rewind: true,
+    cover: true,
+    heightRatio: 0.5,
+    heightRatio: 0.5,
+  }).mount();
+});
 
