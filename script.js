cd997733f08ca0ac806470176cc2d961d602a9dc
Grenish rai <107925840+Grenish@users.noreply.github.com>
2023-03-01 17:18:29 +0530

    Blog contents (#2)
    * Fixes some bugs
    
    * added some images for use
    
    * New page for 404 error, Coming soon and dummy blogs
    
    * Carousel is added
    
    * Photos for carousel
    
    * links used for the material/assets
    
    * carousel css
    
    * script for carousel

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-const searchbtn = document.getElementById('searchBtn');
-const closebtn = document.getElementById('closeBtn');
-const searchbox = document.getElementById('hiddenBox');
+const searchbtn = document.getElementById("searchBtn");
+const closebtn = document.getElementById("closeBtn");
+const searchbox = document.getElementById("hiddenBox");
 
-const subbtn = document.getElementById('subBtn');
-const letterBox = document.getElementById('letterBox');
-const clsLetter = document.getElementById('clsLetter');
+const subbtn = document.getElementById("subBtn");
+const letterBox = document.getElementById("letterBox");
+const clsLetter = document.getElementById("clsLetter");
 
-searchbtn.addEventListener('click', () => {
-    searchbox.style.display = 'block';
+searchbtn.addEventListener("click", () => {
+  searchbox.style.display = "block";
 });
 
-closebtn.addEventListener('click', () => {
-    searchbox.style.display = 'none';
+closebtn.addEventListener("click", () => {
+  searchbox.style.display = "none";
 });
 
-subbtn.addEventListener('click', () => {
-    letterBox.style.display = 'block';
+subbtn.addEventListener("click", () => {
+  letterBox.style.display = "block";
 });
 
-clsLetter.addEventListener('click', () => {
-    letterBox.style.display = 'none';
+clsLetter.addEventListener("click", () => {
+  letterBox.style.display = "none";
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide("#image-carousel", {
+    width: "100vw",
+    height: "60vh",
+    autoplay: true,
+    loop: true,
+    rewind: true,
+    cover: true,
+    heightRatio: 0.5,
+    heightRatio: 0.5,
+  }).mount();
+});
+
